4071ea7c7419c87abc848d89f14665dda0873e49
Mateus <mateus.venancio@basis.com.br>
2023-10-10 21:05:10 -0300

    initializing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+export default function About() {
+    return (
+        <>
+            <p className="text-2xl font-bold">About</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+                aliquid laudantium ducimus iusto necessitatibus dignissimos ut,
+                ullam omnis quo. Laborum reiciendis numquam asperiores hic quod
+                perspiciatis, iusto nihil. Voluptas, eveniet! Lorem ipsum dolor
+                sit amet consectetur adipisicing elit. Nihil aliquid laudantium
+                ducimus iusto necessitatibus dignissimos ut, ullam omnis quo.
+                Laborum reiciendis numquam asperiores hic quod perspiciatis,
+                iusto nihil. Voluptas, eveniet!
+            </p>
+        </>
+    );
+}
