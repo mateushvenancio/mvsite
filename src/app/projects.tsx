e70211c452742f941ae648cb97222a8ad836e579
Mateus <mateus.venancio@basis.com.br>
2023-10-15 23:16:48 -0300

    project page integrated with api

--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -1,21 +1,41 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import Title from './global/title';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCodeBranch } from '@fortawesome/free-solid-svg-icons';
 
+const colors: any = {
+    yellow: {
+        text: 'text-[#402c1b]',
+        background: 'bg-[#fdecc8]',
+    },
+    gray: {
+        text: 'text-[#32302c]',
+        background: 'bg-[#e3e2e0]',
+    },
+    green: {
+        text: 'text-[#1c3829]',
+        background: 'bg-[#dbeddb]',
+    },
+    blue: {
+        text: 'text-[#183347]',
+        background: 'bg-[#d3e5ef]',
+    },
+    default: {
+        text: 'text-[#32302c]',
+        background: 'bg-[#e3e2e0]',
+    },
+};
+
 export default async function Projects() {
-    const response = await fetch(
-        'https://raw.githubusercontent.com/mateushvenancio/projetos-md/main/projects.json'
-    );
+    const response = await fetch('http://localhost:3000/api/projects');
     const projects: Project[] = (await response.json()).projects;
 
     return (
         <>
             <Title title="Projects" />
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                {projects.map((e, i) => {
-                    return <ProjectTile key={i} project={e} />;
+                {projects.map((e) => {
+                    return <ProjectTile key={e.id} project={e} />;
                 })}
             </div>
         </>
@@ -26,15 +46,7 @@ function ProjectTile({ project }: { project: Project }) {
     return (
         <div className="shadow-md hover:shadow-lg rounded-lg p-4 transition-all cursor-default">
             <div className="flex gap-2 items-center">
-                <Image
-                    src={project.icon}
-                    width={20}
-                    height={20}
-                    alt={project.title}
-                    className="aspect-square"
-                />
-                <p className="text-lg">{project.title}</p>
-                <div className="grow"></div>
+                <p className="text-lg grow">{project.title}</p>
                 <Link href={project.link}>
                     <FontAwesomeIcon
                         icon={faCodeBranch}
@@ -43,6 +55,22 @@ function ProjectTile({ project }: { project: Project }) {
                 </Link>
             </div>
             <p className="text-sm line-clamp-2">{project.description}</p>
+            <div className="flex flex-wrap gap-2 mt-2">
+                {project.tags.map((e, i) => {
+                    return <Tag key={i} tag={e} />;
+                })}
+            </div>
+        </div>
+    );
+}
+
+function Tag({ tag }: { tag: ProjectTag }) {
+    const text = colors[tag.color].text ?? colors.default.text;
+    const bg = colors[tag.color].background ?? colors.default.background;
+
+    return (
+        <div className={`text-sm w-fit px-1.5 rounded ${text} ${bg}`}>
+            {tag.name}
         </div>
     );
 }
